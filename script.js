65145403a7a46c9cd88514ab2878a94e391e241e
Your Name <seoexpert1000100u@gmail.com>
2023-11-24 03:46:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+gsap.to ("#box1",{
+   x:400,
+   duration:2,
+   delay:5,
+   backgroundColor: "brown",
+   rotate:360,
+   scale:0.5,
+   
+   
+}) 
+gsap.to ("h1",{
+    x:700,
+    duration:1,
+    delay:1,
+    scale:1.3,
+    color:"crimson",
+    stagger:1,
+    
+ }) 
+ gsap.to ("#ball1",{
+    visibility: "visible",   
+    duration:1,
+    delay:7,
+    y:-300,
+    repeat:-1,
+    yoyo:true,
+    backgroundColor: "brown",
+      
+ }) 
+ gsap.to ("#ball2",{
+    visibility: "visible",   
+    duration:1.01,
+    delay:7,
+    y:-300,
+    repeat:-1,
+    yoyo:true,
+    backgroundColor: "brown",
+      
+ }) 
+ 
+ 
+     
